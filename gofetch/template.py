b5e36ee93d51c790014f51d031a2f1aed843078a
Icaro Medeiros <icaro.medeiros@gmail.com>
2016-02-15 21:08:25 -0200

    add render string template -- tested and working nice, yey

--- gofetch/template.py
+++ gofetch/template.py
@@ -1,7 +1,28 @@
-from jinja2 import Environment, PackageLoader
+from jinja2 import Environment, PackageLoader,\
+    Template, TemplateNotFound
 
 jinja_env = Environment(loader=PackageLoader('gofetch', 'templates'))
 
 
-def render(template_string_or_file, param_dict):
-    pass
+def render(template_str_or_file, param_dict):
+    try:
+        template = jinja_env.get_template()
+        return _render_file_template(template, param_dict)
+    except TemplateNotFound:
+        # TODO valid template string?
+        return _render_string_template(template_str_or_file, param_dict)
+
+
+def _render_string_template(template_str, param_dict):
+    # TODO log
+    tmplt = Template(template_str)
+    return _do_render(tmplt, param_dict)
+
+
+def _render_file_template(template_obj, param_dict):
+    # TODO log
+    return _do_render(template_obj, param_dict)
+
+
+def _do_render(template_obj, param_dict):
+    return template_obj.render(**param_dict)
--- tests/test_template.py
+++ tests/test_template.py
@@ -6,11 +6,10 @@ from unittest import TestCase
 class RenderTemplateTestCase(TestCase):
 
     SIMPLE_TEMPLATE = """
-    SELECT {{ fields }}
-    FROM {{ database_and_table }}
-    WHERE {{ conditions }}
-    ;
-    """
+SELECT {{ fields }}
+FROM {{ database_and_table }}
+WHERE {{ condition }}
+;"""
 
     SIMPLE_PARAM_DICT = {
         "fields": "*",
@@ -18,11 +17,15 @@ class RenderTemplateTestCase(TestCase):
         "condition": "b > 1"
     }
 
-    def test_simple_rendering(self):
+    def test_render_string(self):
         EXPECTED = """
-        SELECT {{ fields }}
-        FROM {{ database_and_table }}
-        WHERE {{ conditions }}
-        """
-        result = template.render(self.SIMPLE_TEMPLATE, self.SIMPLE_PARAM_DICT)
+SELECT *
+FROM test_db.test
+WHERE b > 1
+;"""
+        result = template._render_string_template(self.SIMPLE_TEMPLATE, self.SIMPLE_PARAM_DICT)
         self.assertEquals(EXPECTED, result)
+
+    def test_render_file(self):
+        # 'simple_template.sql'
+        self.fail("test not implemented")
